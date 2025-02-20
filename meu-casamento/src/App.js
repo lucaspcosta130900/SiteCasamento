2eb18fedeaee44bdf72ed9eb072c144d6d584090
Lucas Paiva <lucas.costa@solutis.com.br>
2025-02-20 17:48:15 -0300

    incluindo projeto react

---
+++ meu-casamento/src/App.js
@@ -0,0 +1,17 @@
+   // src/App.js
+   import React from 'react';
+   import './App.css';
+
+   function App() {
+     return (
+       <div className="App">
+         <header className="App-header">
+           <h1>Bem-vindos ao nosso casamento!</h1>
+           <p>Estamos muito felizes em compartilhar este momento especial com você.</p>
+           <img src="sua-imagem.jpg" alt="Imagem dos noivos" />
+         </header>
+       </div>
+     );
+   }
+
+   export default App;
