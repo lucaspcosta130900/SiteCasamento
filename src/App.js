86f375c9b5ac029a95a239838f3a7898c32fe005
Lucas Paiva <lucas.costa@solutis.com.br>
2025-02-21 16:20:59 -0300

    ajustando ainda mais transicoes e alinhamento mobile

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
    // src/App.js
    import React from 'react';
-   import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-   import Navbar from './components/Navbar';
-   import Home from './pages/Home';
-   import SobreNos from './pages/SobreNos';
-   import Confirmacao from './pages/Confirmacao';
-   import Local from './pages/Local';
+   import { HashRouter as Router } from 'react-router-dom';
+   import AppRoutes from './AppRoutes';
    import './App.css';
 
    function App() {
      return (
        <Router>
          <div className="App">
-           <Navbar />
-           <Routes>
-             <Route path="/" element={<Home />} />
-             <Route path="/sobre" element={<SobreNos />} />
-             <Route path="/confirmacao" element={<Confirmacao />} />
-             <Route path="/local" element={<Local />} />
-           </Routes>
+           <AppRoutes />
          </div>
        </Router>
      );
