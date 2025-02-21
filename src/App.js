a0577d90e56ad25502870198188fc1cb9c0bd1d5
Lucas Paiva <lucas.costa@solutis.com.br>
2025-02-21 14:33:49 -0300

    corrigindo 404

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
    // src/App.js
    import React from 'react';
-   import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+   import { HashRouter as Router, Routes, Route } from 'react-router-dom';
    import Navbar from './components/Navbar';
    import Home from './pages/Home';
    import SobreNos from './pages/SobreNos';
