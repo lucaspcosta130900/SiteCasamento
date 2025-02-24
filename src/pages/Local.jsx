256b75587415d033212d1135765e668cf33ec578
Lucas Paiva <lucas.costa@solutis.com.br>
2025-02-23 23:20:38 -0300

    melhorando visualizacao mobile e consertando google maps api

--- src/pages/Local.jsx
+++ src/pages/Local.jsx
@@ -13,7 +13,7 @@ function Local() {
     // Verifica se o script do Google Maps já está carregado
     if (!window.google) {
       const script = document.createElement('script');
-      script.src = `https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY`;
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}`;
       script.async = true;
       script.defer = true;
       script.onload = () => {
